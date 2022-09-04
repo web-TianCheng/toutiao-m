5d22623e2f3bf1fe1273c238d8e05ec14bc85750
litiancheng <1306557974@qq.com>
2022-09-04 15:33:40 +0800

    引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     <i class="toutiao icon-toutiaodianzan"></i>
+    <van-cell-group>
+  <van-cell title="单元格" value="内容" />
+  <van-cell title="单元格" value="内容" label="描述信息" />
+</van-cell-group>
     <router-view/>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vant from 'vant'
 import router from './router'
 import store from './store'
+import 'vant/lib/index.css'
 import '@/style/index.less'
-
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
